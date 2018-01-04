fb2a015a8251d7b55d08c4965690711a08b91de2
Wei-Chen Chen <wccsnow@gmail.com>
2018-01-04 17:02:20 +0000

    version 0.3-0

---
+++ R/get_conf_zmq.r
@@ -0,0 +1,180 @@
+#' ZMQ Flags
+#' 
+#' ZMQ Flags
+#' 
+#' \code{get.zmq.cppflags()} gets CFLAGS or CPPFLAGS
+#'
+#' \code{get.zmq.ldflags()} gets LDFLAGS for libzmq.so, libzmq.dll, or libzmq.*.dylib
+#'
+#' \code{get.pbdZMQ.ldflags()} gets LDFLAGS for pbdZMQ.so or pbdZMQ.dll
+#' 
+#' \code{test.load.zmq()} tests load libzmq and pbdZMQ shared libraries
+#' 
+#' @param arch 
+#' '' (default) for non-windows or '/i386' and '/ix64' for windows
+#' @param package
+#' the pbdZMQ package 
+#' 
+#' @return flags to compile and link with ZMQ.
+#' 
+#' @author Wei-Chen Chen \email{wccsnow@@gmail.com}.
+#' 
+#' @references ZeroMQ/4.1.0 API Reference:
+#' \url{http://api.zeromq.org/4-1:_start}
+#' 
+#' Programming with Big Data in R Website: \url{http://r-pbd.org/}
+#' 
+#' @examples
+#' \dontrun{
+#' get.zmq.cppflags(arch = '/i386')
+#' get.zmq.ldflags(arch = '/x64')
+#' get.pbdZMQ.ldflags(arch = '/x64')
+#' test.load.zmq(arch = '/x64')
+#' }
+#' 
+#' @keywords compile
+#' @rdname zz_zmq_flags
+#' @name ZMQ Flags
+NULL
+
+
+
+#' @rdname zz_zmq_flags
+#' @export
+get.zmq.ldflags <- function(arch = '', package = "pbdZMQ"){
+  if(arch == "/i386" || arch == "/x64"){
+    file.name <- paste("./libs", arch, "/", sep = "")
+    dir.path <- tools::file_path_as_absolute(
+                  system.file(file.name, package = package))
+    zmq.ldflags <- paste("-L\"", dir.path, "\" -lzmq", sep = "")
+  } else{
+    ### For non windows system.
+    file.name <- paste("./etc", arch, "/Makeconf", sep = "")
+    file.path <- tools::file_path_as_absolute(
+                   system.file(file.name, package = package))
+    ret <- scan(file.path, what = character(), sep = "\n", quiet = TRUE)
+
+    ### Check if external zmq is used.
+    arg <- "EXTERNAL_ZMQ_LDFLAGS"
+    id <- grep(paste("^", arg, " = ", sep = ""), ret)
+    ext.zmq.ld <- gsub(paste("^", arg, " = (.*)", sep = ""), "\\1", ret[id[1]])
+
+    ### Check if internal zmq is used.
+    arg <- "ENABLE_INTERNAL_ZMQ"
+    id <- grep(paste("^", arg, " = ", sep = ""), ret)
+    en.int.zmq <- gsub(paste("^", arg, " = (.*)", sep = ""), "\\1", ret[id[1]])
+
+    ### Check which zmq should be used.
+    if(ext.zmq.ld == "" || en.int.zmq == "yes"){
+      file.name <- paste("./libs", arch, "/", sep = "")
+      dir.path <- tools::file_path_as_absolute(
+                    system.file(file.name, package = package))
+      if(Sys.info()[['sysname']] == "Darwin"){
+        lib.osx <- list.files(dir.path, pattern = "libzmq\\.(.*)\\.dylib")
+        i.ver <- gsub("libzmq\\.(.*)\\.dylib", "\\1", lib.osx)
+	i.ver <- max(as.integer(i.ver))
+        zmq.ldflags <- paste("-L", dir.path, " -lzmq.", i.ver, sep = "")
+      } else{
+        zmq.ldflags <- paste("-L", dir.path, " -lzmq", sep = "")
+      }
+    } else{
+      zmq.ldflags <- ext.zmq.ld
+    }
+  }
+
+  ### Cat back to "Makevars".
+  cat(zmq.ldflags)
+
+  invisible(zmq.ldflags)
+} # End of get.zmq.ldflags().
+
+
+
+#' @rdname zz_zmq_flags
+#' @export
+get.zmq.cppflags <- function(arch = '', package = "pbdZMQ"){
+  if(arch == "/i386" || arch == "/x64"){
+    file.name <- paste("./zmq", arch, "/include", sep = "")
+    dir.path <- tools::file_path_as_absolute(
+                  system.file(file.name, package = package))
+    zmq.cppflags <- paste("-I\"", dir.path, "\"", sep = "")
+  } else{
+    ### For non windows system.
+    file.name <- paste("./etc", arch, "/Makeconf", sep = "")
+    file.path <- tools::file_path_as_absolute(
+                   system.file(file.name, package = package))
+    ret <- scan(file.path, what = character(), sep = "\n", quiet = TRUE)
+
+    ### Check if external zmq is used.
+    arg <- "EXTERNAL_ZMQ_INCLUDE"
+    id <- grep(paste("^", arg, " = ", sep = ""), ret)
+    ext.zmq.inc <- gsub(paste("^", arg, " = (.*)", sep = ""), "\\1", ret[id[1]])
+
+    ### Check if internal zmq is used.
+    arg <- "ENABLE_INTERNAL_ZMQ"
+    id <- grep(paste("^", arg, " = ", sep = ""), ret)
+    en.int.zmq <- gsub(paste("^", arg, " = (.*)", sep = ""), "\\1", ret[id[1]])
+
+    ### Check which zmq should be used.
+    if(ext.zmq.inc == "" || en.int.zmq == "yes"){
+      file.name <- paste("./zmq", arch, "/include/", sep = "")
+      dir.path <- tools::file_path_as_absolute(
+                    system.file(file.name, package = package))
+      zmq.cppflags <- paste("-I", dir.path, sep = "")
+    } else{
+      zmq.cppflags <- ext.zmq.inc
+    }
+  }
+
+  ### Cat back to "Makevars".
+  cat(zmq.cppflags)
+
+  invisible(zmq.cppflags)
+} # End of get.zmq.cppflags().
+
+
+
+#' @rdname zz_zmq_flags
+#' @export
+test.load.zmq <- function(arch = '', package = "pbdZMQ"){
+  file.name <- paste("./libs", arch, "/", sep = "")
+  dir.path <- tools::file_path_as_absolute(
+                system.file(file.name, package = package))
+
+  files <- c("libzmq.so", "libzmq.so.dSYM", "libzmq.dylib", "libzmq.4.dylib",
+             "libzmq.5.dylib", "libzmq.dll")
+  for(i.file in files){
+    fn <- paste(dir.path, "/", i.file, sep = "")
+    if(file.exists(fn)){
+      ret <- try(dyn.load(fn, local = FALSE), silent = TRUE)
+      print(ret)
+      cat("\n")
+    }
+  }
+
+  invisible(NULL)
+} # End of test.load.zmq().
+
+
+
+#' @rdname zz_zmq_flags
+#' @export
+get.pbdZMQ.ldflags <- function(arch = '', package = "pbdZMQ"){
+  file.name <- paste("./libs", arch, "/", sep = "")
+  dir.path <- tools::file_path_as_absolute(
+                system.file(file.name, package = package))
+
+  if(arch == "/i386" || arch == "/x64"){
+    pbdZMQ.ldflags <- paste(dir.path, "/pbdZMQ.dll", sep = "")
+  } else{
+    pbdZMQ.ldflags <- paste(dir.path, "/pbdZMQ.so", sep = "")
+  }
+
+  ### Cat back to "Makevars".
+  cat(pbdZMQ.ldflags)
+
+  invisible(pbdZMQ.ldflags)
+} # End of get.pbdZMQ.ldflags().
+
+
+
--- src/install.libs.R
+++ src/install.libs.R
@@ -2,54 +2,74 @@
 ### For libs
 files <- c("pbdZMQ.so", "pbdZMQ.so.dSYM", "pbdZMQ.dylib", "pbdZMQ.dll",
            "symbols.rds",
-           "libzmq.so", "libzmq.so.dSYM", "libzmq.4.dylib",
-           "libzmq.dll")
+           "libzmq.so", "libzmq.so.dSYM", "libzmq.dll")
 files <- files[file.exists(files)]
+lib.osx <- list.files(pattern = "libzmq\\.(.*)\\.dylib")
+files <- c(files, lib.osx)
+
 if(length(files) > 0){
   libsarch <- if (nzchar(R_ARCH)) paste("libs", R_ARCH, sep='') else "libs"
   dest <- file.path(R_PACKAGE_DIR, libsarch)
   dir.create(dest, recursive = TRUE, showWarnings = FALSE)
   file.copy(files, dest, overwrite = TRUE, recursive = TRUE)
 
-  ### For Mac OSX 10.10 Yosemite and when "internal ZMQ" is asked.
+  ### For Mac OSX and when "internal ZMQ" is asked.
   ### Overwrite RPATH from the shared library installed to the destination.
-  if(Sys.info()[['sysname']] == "Darwin" && ("libzmq.4.dylib" %in% files)){
+  if(Sys.info()[['sysname']] == "Darwin"){
     cmd.int <- system("which install_name_tool", intern = TRUE)
+    cmd.ot <- system("which otool", intern = TRUE) 
     fn.pbdZMQ.so <- file.path(dest, "pbdZMQ.so")
-    fn.libzmq.4.dylib <- file.path(dest, "libzmq.4.dylib")
 
-    if(length(grep("install_name_tool", cmd.int)) > 0 &&
-       file.exists(fn.pbdZMQ.so) &&
-       file.exists(fn.libzmq.4.dylib)){
+    if(length(lib.osx) != 1){
+      print(lib.osx)
+      stop("None or more than one libzmq.*.dylib are found.")
+    } else{
+      fn.libzmq.dylib <- file.path(dest, lib.osx)
 
-      cmd.ot <- system("which otool", intern = TRUE) 
-      if(length(grep("otool", cmd.ot)) > 0){
+      if(file.exists(fn.pbdZMQ.so) && file.exists(fn.libzmq.dylib)){
+        ### For pbdZMQ.so
         rpath <- system(paste(cmd.ot, " -L ", fn.pbdZMQ.so, sep = ""),
                         intern = TRUE)
-        cat("\nBefore install_name_tool:\n")
+        cat("\nBefore install_name_tool (install.libs.R & pbdZMQ.so):\n")
         print(rpath)
-      }
 
-      org <- file.path(getwd(), "zmq/lib/libzmq.4.dylib")
-      cmd <- paste(cmd.int, " -change ", org, " ", fn.libzmq.4.dylib, " ",
-                   fn.pbdZMQ.so, sep = "")
-      cat("\nIn install_name_tool:\n")
-      print(cmd) 
-      system(cmd)
+        cmd <- paste(cmd.int, " -id ", fn.pbdZMQ.so, " ",
+                     fn.pbdZMQ.so, sep = "")
+        cat("\nIn install_name_tool (install.libs.R & pbdZMQ.so & id):\n")
+        print(cmd) 
+        system(cmd)
 
-      if(length(grep("otool", cmd.ot)) > 0){
+        str.lib <- paste("zmq/lib/", lib.osx, sep = "")
+        org <- file.path(getwd(), str.lib)
+        cmd <- paste(cmd.int, " -change ", org, " @loader_path/", lib.osx, " ",
+                     fn.pbdZMQ.so, sep = "")
+        cat("\nIn install_name_tool (install.libs.R & pbdZMQ.so & path):\n")
+        print(cmd) 
+        system(cmd)
+
         rpath <- system(paste(cmd.ot, " -L ", fn.pbdZMQ.so, sep = ""),
                         intern = TRUE)
-        cat("\nAfter install_name_tool:\n")
+        cat("\nAfter install_name_tool (install.libs.R & pbdZMQ.so):\n")
+        print(rpath)
+
+        ### For libzmq.dylib
+        rpath <- system(paste(cmd.ot, " -L ", fn.libzmq.dylib, sep = ""),
+                        intern = TRUE)
+        cat("\nBefore install_name_tool (install.libs.R & libzmq.dylib):\n")
+        print(rpath)
+
+        cmd <- paste(cmd.int, " -id ", fn.libzmq.dylib, " ",
+                     fn.libzmq.dylib, sep = "")
+        cat("\nIn install_name_tool (install.libs.R & libzmq.dylib & id):\n")
+        print(cmd) 
+        system(cmd)
+
+        rpath <- system(paste(cmd.ot, " -L ", fn.libzmq.dylib, sep = ""),
+                        intern = TRUE)
+        cat("\nAfter install_name_tool (install.libs.R & libzmq.dylib):\n")
         print(rpath)
       }
     }
-
-    # fn.libzmq.dylib <- file.path(dest, "libzmq.dylib")
-    # cmd <- paste("ln -s ", fn.libzmq.4.dylib, " ", fn.libzmq.dylib, sep = "")
-    # cat("\nCreate a symbolic link:\n")
-    # print(cmd) 
-    # system(cmd)
   }
 }
 
@@ -62,14 +82,14 @@ if(file.exists(file)){
   file.copy(file, dest, overwrite = TRUE)
 }
 
-### For zmq include
+### For zmq and pbdZMQ include
 dir.zmq <- "./zmq"
 if(file.exists(dir.zmq)){
   libarch <- if (nzchar(R_ARCH)) paste("zmq", R_ARCH, sep='') else "zmq"
   dest <- file.path(R_PACKAGE_DIR, libarch)
   dir.create(dest, recursive = TRUE, showWarnings = FALSE)
-  # files <- paste(dir.zmq, c("/include", "/lib") , sep = "")
   files <- paste(dir.zmq, "/include" , sep = "")
+  file.copy("./R_zmq.h", files)
   file.copy(files, dest, overwrite = TRUE, recursive = TRUE)
 }
 
