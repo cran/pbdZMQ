530a5368e8bd6e7c434191dbd57503be80a13a1d
Wei-Chen Chen <wccsnow@gmail.com>
2021-10-29 08:00:02 +0000

    version 0.3-6

--- tests/send_recv.R
+++ tests/send_recv.R
@@ -2,16 +2,33 @@ suppressPackageStartupMessages(library(pbdZMQ))
 
 ### zmq interface
 
-# Server
-server_context = zmq.ctx.new()
-server_socket = zmq.socket(server_context, .pbd_env$ZMQ.ST$REP)
-zmq.bind(server_socket, "tcp://*:55555")
+### In general and separate files
+# server_context = zmq.ctx.new()
+# server_socket = zmq.socket(server_context, .pbd_env$ZMQ.ST$REP)
+# client_context = zmq.ctx.new()
+# client_socket = zmq.socket(client_context, .pbd_env$ZMQ.ST$REQ)
+### Server file
+# zmq.setsockopt(server_socket, .pbd_env$ZMQ.SO$CONNECT_TIMEOUT, 1000L)
+# zmq.setsockopt(server_socket, .pbd_env$ZMQ.SO$RCVTIMEO, 1000L)
+# zmq.setsockopt(server_socket, .pbd_env$ZMQ.SO$SNDTIMEO, 1000L)
+# zmq.bind(server_socket, "tcp://*:55555")
+### client file
+# zmq.setsockopt(client_socket, .pbd_env$ZMQ.SO$CONNECT_TIMEOUT, 1000L)
+# zmq.setsockopt(client_socket, .pbd_env$ZMQ.SO$RCVTIMEO, 1000L)
+# zmq.setsockopt(client_socket, .pbd_env$ZMQ.SO$SNDTIMEO, 1000L)
+# zmq.connect(client_socket, "tcp://localhost:55555")
 
-# Client
-client_context = zmq.ctx.new()
-client_socket = zmq.socket(client_context, .pbd_env$ZMQ.ST$REQ)
-zmq.connect(client_socket, "tcp://localhost:55555")
+### For CRAN testing in local (the same process) only to avoid block
+cran_context = zmq.ctx.new()
+server_socket = zmq.socket(cran_context, .pbd_env$ZMQ.ST$REP)
+client_socket = zmq.socket(cran_context, .pbd_env$ZMQ.ST$REQ)
+### Server
+zmq.bind(server_socket, "inproc://#1")
+### client
+zmq.connect(client_socket, "inproc://#1")
 
+
+### Test pbdZMQ
 tester = function(indata)
 {
   zmq.msg.send(indata, client_socket)
