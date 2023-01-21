12ddb3050cc75240925cb5a3ee2527dc9c21abdb
Wei-Chen Chen <wccsnow@gmail.com>
2023-01-21 19:40:02 +0000

    version 0.3-9

--- src/R_zmq_utility.c
+++ src/R_zmq_utility.c
@@ -33,11 +33,12 @@ SEXP R_zmq_version(void){
 	zmq_version(&major, &minor, &patch);
 	//Rprintf("Current ZeroMQ version is %d.%d.%d\n", major, minor, patch);
 
-	chars = sprintf(ver, "%d.%d.%d", major, minor, patch);
+        /* R-devel on around Dec. 24, 2022 starting to warn the line below. */
+	// chars = sprintf(ver, "%d.%d.%d", major, minor, patch);
+	chars = snprintf(ver, 36, "%d.%d.%d", major, minor, patch);
 
 	ret = PROTECT(allocVector(STRSXP, 1));
 	SET_STRING_ELT(ret, 0, mkCharLen(ver, chars));
-
 	UNPROTECT(1);
 	return(ret);
 } /* End of R_zmq_version(). */
